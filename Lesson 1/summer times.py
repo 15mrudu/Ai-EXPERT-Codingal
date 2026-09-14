f75a8e43e8d741af246c343385ececb09eec4a0d
15mrudu <mrudula15selokar@gmail.com>
2026-09-14 09:16:32 +0530

    summer

---
+++ Lesson 1/summer times.py
@@ -0,0 +1,8 @@
+month = input("Enter the month: ")
+
+if month == "April" or month == "May" or month == "June":
+    print("It is summer time! ☀️")
+else:
+    print("It is not summer time.")
+
+print("Congratulations! You completed the activity! ")
