06a6f4ae35980242ff38a921fe53d340799f07d0
15mrudu <mrudula15selokar@gmail.com>
2026-09-14 09:22:24 +0530

    Operator

---
+++ Lesson 1/identify operator.py
@@ -0,0 +1,14 @@
+operator = input("Enter an operator (+, -, *, /): ")
+
+if operator == "+":
+    print("You entered Addition operator.")
+elif operator == "-":
+    print("You entered Subtraction operator.")
+elif operator == "*":
+    print("You entered Multiplication operator.")
+elif operator == "/":
+    print("You entered Division operator.")
+else:
+    print("Invalid operator.")
+
+print("Congratulations! You completed the activity! ")
