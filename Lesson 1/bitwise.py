96b8d366ab77b24708b6ab1970515e6db47f7b14
15mrudu <mrudula15selokar@gmail.com>
2026-09-14 09:24:05 +0530

    Bitwise

---
+++ Lesson 1/bitwise.py
@@ -0,0 +1,22 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+# Bitwise AND
+print("Bitwise AND (&):", num1 & num2)
+
+# Bitwise OR
+print("Bitwise OR (|):", num1 | num2)
+
+# Bitwise XOR
+print("Bitwise XOR (^):", num1 ^ num2)
+
+# Bitwise NOT
+print("Bitwise NOT (~) of first number:", ~num1)
+
+# Left Shift
+print("Left Shift (<<):", num1 << 1)
+
+# Right Shift
+print("Right Shift (>>):", num1 >> 1)
+
+print("\nCongratulations! You completed the Bitwise Operators activity! ")
