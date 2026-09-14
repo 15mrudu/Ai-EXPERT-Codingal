d0f68de66df79739a9e4c911cc9937a65e0f7ed8
15mrudu <mrudula15selokar@gmail.com>
2026-09-14 08:38:25 +0530

    birthday

---
+++ Lesson 1/birthday.py
@@ -0,0 +1,14 @@
+
+# PART 1: Ask for the person's name
+name = input("Enter your name: ")
+
+# PART 2: Ask for the birthday
+birthday = input("Enter your birthday (DD/MM/YYYY): ")
+
+# PART 3: Store the birthday
+print("\nBirthday Information")
+print("Name:", name)
+print("Birthday:", birthday)
+
+# PART 4: Display a birthday message
+print("Happy Birthday, " + name + "!")
