d11df9796f51ad0feefbfbacbd0b5cb04769a917
15mrudu <mrudula15selokar@gmail.com>
2026-09-14 08:41:23 +0530

    Congrats

---
+++ Lesson 1/Congrts.py
@@ -0,0 +1,16 @@
+
+# Ask for the person's name
+name = input("Enter your name: ")
+
+# Ask for the birthday
+birthday = input("Enter your birthday (DD/MM/YYYY): ")
+
+# Display the birthday information
+print("\nBirthday Information")
+print("Name:", name)
+print("Birthday:", birthday)
+
+# Congratulations message
+print("\nCongratulations,", name + "!")
+print("You have successfully completed the Birthday Storage activity!")
+print("Keep coding and keep learning! 🎉")
