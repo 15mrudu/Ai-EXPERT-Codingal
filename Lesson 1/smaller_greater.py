6546bdb907415a599fa478e8fae24407660d5168
15mrudu <mrudula15selokar@gmail.com>
2026-09-14 09:15:24 +0530

    even_odd

--- Lesson 1/smaller_greater.py
+++ Lesson 1/smaller_greater.py
@@ -3,7 +3,7 @@ secret_number = 7
 number = int(input("Enter a number: "))
 
 if number == secret_number:
-    print("Congratulations! You guessed the correct number! 🎉")
+    print("Congratulations! You guessed the correct number! ")
 else:
     print("Sorry! That is not the correct number.")
 
