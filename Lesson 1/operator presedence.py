5a1b4409faf369c087289e4ed27b4b903471b2c9
15mrudu <mrudula15selokar@gmail.com>
2026-09-14 09:27:43 +0530

    operator presedence

---
+++ Lesson 1/operator presedence.py
@@ -0,0 +1,12 @@
+# Ask the user for three numbers
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+c = int(input("Enter the third number: "))
+
+# Calculate using different operators
+result = a + b * c
+
+# Display the result
+print("Result of a + b * c is:", result)
+
+print("Congratulations! You completed the Operator Precedence activity! ")
