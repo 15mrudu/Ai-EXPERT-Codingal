ac7c752bd7612bfbc44055fc42432c0a2d34ea11
15mrudu <mrudula15selokar@gmail.com>
2026-09-14 09:25:49 +0530

    ascii

---
+++ Lesson 1/ascii.py
@@ -0,0 +1,10 @@
+character = input("Enter a character: ")
+
+# Find the ASCII value
+ascii_value = ord(character)
+
+# Display the result
+print("The ASCII value of", character, "is:", ascii_value)
+
+# Congratulations message
+print("Congratulations! You completed the ASCII Value activity! ")
