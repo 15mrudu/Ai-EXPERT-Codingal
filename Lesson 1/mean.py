7346eaeec26786b078dd4d2d04e3f5b4920c56bf
15mrudu <mrudula15selokar@gmail.com>
2026-09-14 09:41:32 +0530

    mean

---
+++ Lesson 1/mean.py
@@ -0,0 +1,15 @@
+# Ask the user to enter five numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+num4 = float(input("Enter the fourth number: "))
+num5 = float(input("Enter the fifth number: "))
+
+# Calculate the mean
+mean = (num1 + num2 + num3 + num4 + num5) / 5
+
+# Display the result
+print("\nThe mean value is:", mean)
+
+# Congratulations message
+print("Congratulations! You completed the Mean Value activity! ")
