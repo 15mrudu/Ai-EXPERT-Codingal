e1af10b9f878b538fe3c5fba90cc740012b3d924
15mrudu <mrudula15selokar@gmail.com>
2026-09-14 09:13:56 +0530

    smaller or greater

---
+++ Lesson 1/smaller_greater.py
@@ -0,0 +1,10 @@
+secret_number = 7
+
+number = int(input("Enter a number: "))
+
+if number == secret_number:
+    print("Congratulations! You guessed the correct number! 🎉")
+else:
+    print("Sorry! That is not the correct number.")
+
+print("Activity completed!")
