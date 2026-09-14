e51eb83890486fdb6eb55c0c401f643b25dc81a7
15mrudu <mrudula15selokar@gmail.com>
2026-09-14 09:09:45 +0530

    sq root

---
+++ Lesson 1/sq root.py
@@ -0,0 +1,10 @@
+number = float(input("Enter a number: "))
+
+# Calculate the square root
+square_root = number ** 0.5
+
+# Display the result
+print("\nThe square root of", number, "is:", square_root)
+
+# Congratulations message
+print("\nCongratulations! You successfully calculated the square root! ")
